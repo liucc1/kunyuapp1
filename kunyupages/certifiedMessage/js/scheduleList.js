181274335ba46e7568ac58862ee779c2061c6250
740524615@qq.com <tong0303LXT>
2018-03-01 11:16:04 +0800

    列表

--- kunyupages/certifiedMessage/js/scheduleList.js
+++ kunyupages/certifiedMessage/js/scheduleList.js
@@ -2,7 +2,32 @@ mui.init({
 	swipeBack: false
 });
 mui.plusReady(function(){
-	
+//	eg.getCsrf();
+//	eg.getToken("68720a30",function(data) {
+//		var s=data.split('"');
+//		var csrf=s[s.length-2];
+//		console.log("未登录时获取_csrf==="+csrf);
+////		isToLogin(csrf);
+//	});
+	var csrf=localStorage.getItem("csrf");
+	var params = {
+		"_csrf":csrf,
+		"page":"1",
+		"limit":"4",
+//		"password":$("#pwd1").val().trim(),
+//		"city":$("#cityCode").val().trim()
+	}
+	eg.postAjax("customer/list", params, function(data) {
+			alert("dddd");
+			
+//			alert(data.total);
+	}
+	,function(data){
+//		$("#oBtn").removeAttr("disabled");
+		//alert(data);
+		if(data=="403") eg.getCsrf();
+	}
+	);
 })
 mui('#pendingList').on('tap', 'ul', function() {
 	mui.openWindow({
