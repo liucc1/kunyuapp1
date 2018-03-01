ee098acb136dc05f944452638041dfef39d5320c
740524615@qq.com <tong0303LXT>
2018-03-01 13:48:55 +0800

    e

--- kunyupages/certifiedMessage/js/pendingList.js
+++ kunyupages/certifiedMessage/js/pendingList.js
@@ -2,7 +2,30 @@ mui.init({
 	swipeBack: false
 });
 mui.plusReady(function(){
-	
+	var csrf=localStorage.getItem("csrf");
+	var params = {
+		"_csrf":csrf,
+		"page":"1",
+		"limit":"4",
+//		"password":$("#pwd1").val().trim(),
+//		"city":$("#cityCode").val().trim()
+	}
+	plus.nativeUI.showWaiting();
+	eg.postAjax("customer/list", params, function(data) {
+			plus.nativeUI.closeWaiting();
+//			alert(data.msg);
+//			alert(data.total);
+//			alert(data.code);		
+			if(data.code=="0"){
+				var arr = JSON.stringify(data.rows);
+				alert(arr);
+			}
+//			alert(data.total);
+	}
+	,function(data){
+//		alert(data);
+		if(data=="403") eg.getCsrf();
+	});
 })
 mui('#productlist').on('tap', 'ul', function() {
 	mui.openWindow({
