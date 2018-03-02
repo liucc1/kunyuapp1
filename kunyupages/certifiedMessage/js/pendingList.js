28a3af41f1077e3ea086cae12a9d632de21ce6e1
740524615@qq.com <tong0303LXT>
2018-03-02 09:45:17 +0800

    d

--- kunyupages/certifiedMessage/js/pendingList.js
+++ kunyupages/certifiedMessage/js/pendingList.js
@@ -2,28 +2,39 @@ mui.init({
 	swipeBack: false
 });
 mui.plusReady(function(){
+	eg.getCsrf();
 	var csrf=localStorage.getItem("csrf");
 	var params = {
 		"_csrf":csrf,
 		"page":"1",
-		"limit":"4",
-//		"password":$("#pwd1").val().trim(),
-//		"city":$("#cityCode").val().trim()
+		"limit":"4"
 	}
 	plus.nativeUI.showWaiting();
 	eg.postAjax("customer/list", params, function(data) {
-			plus.nativeUI.closeWaiting();
+		plus.nativeUI.closeWaiting();
 //			alert(data.msg);
 //			alert(data.total);
 //			alert(data.code);		
-			if(data.code=="0"){
-				var arr = JSON.stringify(data.rows);
-				alert(arr);
+		if(data.code=="0"){
+//			var arr = JSON.stringify(data.rows);
+			var arr = data.rows;
+			alert(arr);
+			for(var index in arr){
+				var custList = data.arr[index];
+				$(custList).each(function(key,val){
+					var html = '<ul class="mui-table-view mui-table-view-chevron ulTop">'
+					html += '<li class="mui-table-view-cell mui-media" data-list='+JSON.stringify(val)+'>';
+					html += '<a class="mui-navigate-right font20">';
+					html += '<div class="mui-table">';
+					html += '<div class="mui-table-cell mui-col-xs-10 font14">';
+					html += '<label>'+val.name+'<span class="ml5">:</span><span class="ml10">影像审核不通过</span></label>';
+					html += '<p style="margin-top: 5px;font-size: 12px;">'+val.createTime+'</p>';
+					html += '</div></li></ul>';
+					$('#productlist').append(html);
+				});
 			}
-//			alert(data.total);
-	}
-	,function(data){
-//		alert(data);
+		}
+	},function(data){
 		if(data=="403") eg.getCsrf();
 	});
 })
